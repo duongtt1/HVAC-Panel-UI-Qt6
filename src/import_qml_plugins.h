8b41b82855129efd960efb2c8ae7d01b2919f878
DuongTT1 <kitaro183@gmail.com>
2023-12-29 14:02:08 +0700

    init project

---
+++ src/import_qml_plugins.h
@@ -0,0 +1,15 @@
+#ifndef IMPORT_QML_PLUGINS_H
+#define IMPORT_QML_PLUGINS_H
+
+// Copyright (C) 2023 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR BSD-3-Clause
+
+#include <QtQml/qqmlextensionplugin.h>
+
+Q_IMPORT_QML_PLUGIN(contentPlugin)
+Q_IMPORT_QML_PLUGIN(HVACCustomControlsPlugin)
+Q_IMPORT_QML_PLUGIN(HVACPlugin)
+
+
+
+#endif // IMPORT_QML_PLUGINS_H
